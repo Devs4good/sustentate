15604f31516e93a2766b4daf2fc1adc082dfd31f
Pablo Cavallo <pac@codebox.com.ar>
2017-11-08 22:32:04 -0300

    se corrije el api key

--- app/src/main/java/com/sustentate/app/utils/Constants.java
+++ app/src/main/java/com/sustentate/app/utils/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
     public static final String BASE_URL = "http://sustentate.developersforgood.com/index.php/";
     public static final String CAMERA_SD_PERMISSION = "cameraPermission";
-    public static final String WATSON_API = "3122f7a1e9945f414a37a3d250c12621968370d8";
+    public static final String WATSON_API = "4dc44ef2a620aeab755695cbcdf5a0826a9554d6";
 }
